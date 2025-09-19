f0b4cf0adddeb2b0dfff53deced31d46168a4270
Julie-t-a <julieta.nunez@iteso.mx>
2025-09-19 00:41:51 -0600

    primer commit

--- segundo_siguiente.py
+++ segundo_siguiente.py
@@ -1,15 +1,36 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Julieta Núñez Pacheco
+19 de septiembre de 2025
+Este ejercicio marca la hora si a la original se 
+le suma un segundo
 """
 
-# Declaraciones
-CONSTANTE = valor
+#Entradas
 
-# Entradas
-entrada = input()
+hora=int(input("dame una hora: "))
+minutos = int(input("dame un minuto: "))
+segundos = int(input("dame una segunos: "))
 
-# Proceso
-salida = entrada
+#Procesos
 
-# Salidas
-print(salida)
+segundos = segundos + 1
+if segundos== 60:
+    minutos = minutos + 1
+    segundos = 0
+else:
+    segundos = segundos
+if minutos == 60:
+    hora = hora+1
+    minutos = 0
+else:
+    minutos = minutos
+
+if hora == 24:
+ hora = 0
+else:
+   hora = hora
+
+#Salidas
+
+print("La hora es: ",hora, ":",
+       minutos, ":", segundos)
